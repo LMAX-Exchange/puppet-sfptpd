a4923643d3b513f9c5764dd92c2a26df4c682bb5
Keith Ward <keith.ward@lmax.com>
2021-09-14 16:09:22 +0100

    Remove relative referencing

--- manifests/sync_module/ntp.pp
+++ manifests/sync_module/ntp.pp
@@ -1,4 +1,4 @@
-define sfptpd::sync_module::ntp(
+define sfptpd::sync_module::ntp (
   $ntp_key,
   $priority          = undef,
   $sync_threshold    = undef,
@@ -8,7 +8,7 @@ define sfptpd::sync_module::ntp(
   validate_integer($ntp_poll_interval)
 
   concat::fragment { "ntp_${name}":
-    target  => $::sfptpd::config_file,
+    target  => $sfptpd::config_file,
     content => template("${module_name}/ntp.erb"),
     order   => 50,
   }
